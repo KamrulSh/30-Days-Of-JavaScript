7a41949a08356e20073542fc9d647111bd8e8147
Kamrul Islam Shahin <44506464+shahin-cuet@users.noreply.github.com>
2020-10-25 20:44:52 +0600

    solve of day 10

--- 10_Day_Sets_and_Maps/10_day_starter/scripts/main.js
+++ 10_Day_Sets_and_Maps/10_day_starter/scripts/main.js
@@ -1,2 +1,39 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+const languageArray = [];
+const languageSet = new Set();
+
+// pushed all language array into a single array
+for (const cLang of countries) {
+    languageArray.push(cLang.languages);
+    languageSet.add(cLang.languages);
+}
+console.log(languageArray);
+console.log(languageSet);
+
+const allLanguageInOneArray = languageArray.flat();
+console.log(allLanguageInOneArray);
+
+const allLanguageInOneSet = new Set(allLanguageInOneArray);
+console.log(allLanguageInOneSet);
+
+const languageCount = [];
+for (const language of allLanguageInOneSet) {
+    const filteredLanguage = allLanguageInOneArray.filter(
+        (lang) => language === lang
+    );
+    languageCount.push({ Lang: language, count: filteredLanguage.length });
+}
+console.log(languageCount);
+
+languageCount.sort((a, b) => {
+    return b.count - a.count;
+});
+console.log(languageCount);
+
+const element = new Set();
+function mostSpokenLanguages(languageCount, length) {
+    for (let i = 0; i < length; i++) {
+        element.add(`${languageCount[i].Lang} : ${languageCount[i].count}`);
+    }
+}
+mostSpokenLanguages(languageCount, 10);
+console.log(element);
