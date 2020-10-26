fd1a35538395052ebe12eb37e6abefcf4327efdb
Kamrul Islam Shahin <44506464+shahin-cuet@users.noreply.github.com>
2020-10-26 21:26:49 +0600

    solve of day 11

--- 11_Day_Destructuring_and_spreading/11_day_starter/scripts/main.js
+++ 11_Day_Destructuring_and_spreading/11_day_starter/scripts/main.js
@@ -1,2 +1,55 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+// level 3 ========================>
+
+// #1
+
+const showCountryData = () => {
+    for (const {
+        name,
+        capital,
+        languages,
+        population,
+        flag,
+        currency,
+    } of countries) {
+        console.log(name, capital, languages, population, flag, currency);
+    }
+};
+
+console.log(showCountryData(countries));
+
+// #3
+const students = [
+    ["David", ["HTM", "CSS", "JS", "React"], [98, 85, 90, 95]],
+    ["John", ["HTM", "CSS", "JS", "React"], [85, 80, 85, 80]],
+];
+let stuObj = [];
+let convertArrayToObject = (stu) => {
+    for (s of stu) {
+        stuObj.push({ Name: s[0], skills: s[1], scores: s[2] });
+    }
+    return stuObj;
+};
+console.log(convertArrayToObject(students));
+
+// #4
+const student = {
+    name: "David",
+    age: 25,
+    skills: {
+        frontEnd: [
+            { skill: "HTML", level: 10 },
+            { skill: "CSS", level: 8 },
+            { skill: "JS", level: 8 },
+            { skill: "React", level: 9 },
+        ],
+        backEnd: [
+            { skill: "Node", level: 7 },
+            { skill: "GraphQL", level: 8 },
+        ],
+        dataBase: [{ skill: "MongoDB", level: 7.5 }],
+        dataScience: ["Python", "R", "D3.js"],
+    },
+};
+student.skills.frontEnd.push({ skill: "Bootstrap", level: 8 });
+student.skills.dataScience.push("SQL");
+console.log(student);
