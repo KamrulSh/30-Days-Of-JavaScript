b95f7b3219780aae0da63adbf505131ea1da668b
Kamrul Islam Shahin <44506464+shahin-cuet@users.noreply.github.com>
2020-10-28 22:37:34 +0600

    solve day 12 level 2

--- 12_Day_Regular_expressions/12_day_starter/scripts/main.js
+++ 12_Day_Regular_expressions/12_day_starter/scripts/main.js
@@ -1,2 +1,20 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+// level 2 ========================>
+
+// #1
+
+paragraph = `I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.`;
+removeDot = /[A-Za-z0-9]+/g;
+splitedPara = paragraph.match(removeDot);
+console.log(splitedPara);
+splitedParaSet = new Set(splitedPara);
+console.log(splitedParaSet);
+arr = [];
+for (const s of splitedParaSet) {
+  const filteredArr = splitedPara.filter(one => s === one);
+  arr.push({ word: s, count: filteredArr.length });
+}
+console.log(arr)
+arr.sort((a,b) => {
+  return b.count - a.count ;
+})
+console.log(arr)
